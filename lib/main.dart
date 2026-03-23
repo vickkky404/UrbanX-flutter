813981b27ca7d34fc36ee693595f7410b29051ea
nalin <nalinikantbastia@gmail.com>
2026-03-23 09:49:44 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
-import 'core/router/app_router.dart';
+import 'core/router/app_route.dart';
 
 void main() {
   runApp(
