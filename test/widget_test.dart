63dbcbf9528b04064dfd2621a701e7ec7bfa405c
nalin <nalinikantbastia@gmail.com>
2026-03-23 10:28:19 +0530

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
@@ -13,18 +15,10 @@ import 'package:urban_x/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const UrbanXApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify initial router destination loads splash screen.
+    await tester.pumpAndSettle();
+    expect(find.text('Splash'), findsOneWidget);
   });
 }
