3b2271ca1a65d922a8a0e6111232f8d802c974a1
nalin <nalinikantbastia@gmail.com>
2026-03-23 09:49:36 +0530

    .

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static const Color seedColor = Color(0xFF6750A4);
+  static const String primaryFontFamily = 'Poppins';
 
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     colorSchemeSeed: seedColor,
     brightness: Brightness.light,
-    textTheme: GoogleFonts.poppinsTextTheme(),
+    fontFamily: primaryFontFamily,
   );
 
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     colorSchemeSeed: seedColor,
     brightness: Brightness.dark,
-    textTheme: GoogleFonts.poppinsTextTheme(),
+    fontFamily: primaryFontFamily,
   );
 }
